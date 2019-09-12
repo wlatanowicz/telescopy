ba7414473c1432e1b7445fb701f4387d6b9be914
Wiktor Latanowicz <wiktor@latanowicz.com>
2019-09-12 09:32:06 +0200

    WIP: dome support

--- telescopy/devices/Dome.py
+++ telescopy/devices/Dome.py
@@ -5,3 +5,35 @@ from indi.device.pool import DevicePool
 @DevicePool.register
 class Dome(Driver):
     name = 'DOME'
+
+    general = properties.Group(
+        'GENERAL',
+        vectors=dict(
+            connection=properties.Standard('CONNECTION'),
+            info=properties.TextVector(
+                'INFO',
+                enabled=False,
+                perm=const.Permissions.READ_ONLY,
+                elements=dict(
+                    manufacturer=properties.Text('MANUFACTURER', default='Wiktor Latanowicz'),
+                    ip=properties.Text('IP_ADDRESS', default=settings.DOME_IP),
+                )
+            ),
+            active_device=properties.Standard(
+                'ACTIVE_DEVICES',
+                elements=dict(
+                    camera=properties.Text('ACTIVE_DOME', default=name)
+                )
+            )
+        )
+    )
+    general.connection.connect.onwrite = 'connect'
+
+    position = properties.Group(
+        'POSITION',
+        enabled=False,
+        vectors=dict(
+            position=properties.Standard('ABS_FOCUS_POSITION'),
+        )
+    )
+    position.position.position.onwrite = 'reposition'
