7caf7984ea84b039ee6d14dcf9ca62879dbff646
Wiktor Latanowicz <wiktor@latanowicz.com>
2020-01-10 19:46:53 +0100

    PHD2 inital version

--- telescopy/devices/__init__.py
+++ telescopy/devices/__init__.py
@@ -1,2 +1,3 @@
 from .SonySLTA58 import SonySLTA58
 from .NodeFocuser import NodeFocuser
+from .PHD2 import PHD2
--- telescopy/settings.py
+++ telescopy/settings.py
@@ -8,11 +8,14 @@ def _to_bool(v):
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 HTTP_PORT = 8000
-BASE_HTTP_URL = f'http://localhost:{HTTP_PORT}/'
+BASE_HTTP_URL = f'http://192.168.0.50:{HTTP_PORT}/'
 
 PUB_DIR = os.path.join(BASE_DIR, 'pub')
 
 GPHOTO_PATH = 'gphoto2'
-FOCUSER_IP = '192.168.0.51'
+FOCUSER_IP = '192.168.5.51'
+
+PHD2_IP = '192.168.5.21'
+PHD2_PORT = 4400
 
 ENABLE_SIMULATORS = _to_bool(os.environ.get('ENABLE_SIMULATORS', True))
